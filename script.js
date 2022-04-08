ea27994b146aad9f8e824bb729a49c782862b377
Marcus <marcusadrenaline@gmail.com>
2022-04-08 12:28:56 -0300

    versao beta com todas a regras funcionando e com a verificação de certo ou errado, alem do contador

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 
 let listaGifs = ['imagens/bobrossparrot.gif', 'imagens/bobrossparrot.gif', 'imagens/explodyparrot.gif', 'imagens/explodyparrot.gif', 'imagens/fiestaparrot.gif', 'imagens/fiestaparrot.gif', 'imagens/metalparrot.gif', 'imagens/metalparrot.gif', 'imagens/revertitparrot.gif', 'imagens/revertitparrot.gif', 'imagens/tripletsparrot.gif', 'imagens/tripletsparrot.gif', 'imagens/unicornparrot.gif', 'imagens/unicornparrot.gif'];
 let repetir = true;
+let parrotsSelecionados = [];
+let parrotBack;
+let parrotFront;
+let cont = 0;
 //QUANTIDADE DE JOGADAS
 let qntCartas = Number(prompt("Com quantas cartas quer jogar. Escolher entre 4 a 14 cartas!"));
 
@@ -18,23 +22,46 @@ while (repetir === true){
         repetir = true;
     }
 }
- function parrotClicado(elemento) {
-     if (document.querySelector("#"+elemento.id+" .back.face") !== null){
-        let clicadoF = document.querySelector("#"+elemento.id+" .back.face");
-        clicadoF.classList.add("rodarprafrente");
-        let clicadoV = document.querySelector("#"+elemento.id+" .front.face");
-        clicadoV.classList.add("rodarpratras");
-     }  
-     else if (document.querySelector("#"+elemento.id+" .front.face") !== null) {
-        let clicado = document.querySelector("#"+elemento.id+" .front.face");
-        clicado.classList.add("rodarpratras");
-        let clicadoF = document.querySelector("#"+elemento.id+" .back.face");
-        clicadoF.classList.add("rodarprafrente");
-     }
-     
- }
-jogar();
+// Jogo iniciado
+setTimeout(jogar,200);
 
+ function parrotClicado(elemento) {
+    cont ++;
+    parrotBack = document.querySelector("#"+elemento.id+" .back.face");
+    parrotBack.classList.add("rodarprafrente");
+    parrotFront = document.querySelector("#"+elemento.id+" .front.face");
+    parrotFront.classList.add("rodarpratras");
+    parrotsSelecionados.push(elemento);
+    if (parrotsSelecionados.length == 2){
+        if (parrotsSelecionados[0].id != parrotsSelecionados[1].id){
+            setTimeout(verificar,800);
+        } else {
+            parrotsSelecionados.pop();
+        }
+    }
+ }
+function errar() {
+    for (i = 0; i < parrotsSelecionados.length; i++){
+        parrotBack = document.querySelector("#"+ parrotsSelecionados[i].id + " .back.face");
+        parrotBack.classList.remove("rodarprafrente");
+        parrotFront = document.querySelector("#"+ parrotsSelecionados[i].id + " .front.face");
+        parrotFront.classList.remove("rodarpratras");   
+    }
+   
+ }
+
+ function verificar() {
+    if (parrotsSelecionados[0].innerHTML !== parrotsSelecionados[1].innerHTML){
+        errar();
+        parrotsSelecionados = [];
+    
+    } else if (parrotsSelecionados[0].innerHTML === parrotsSelecionados[1].innerHTML){
+        parrotsSelecionados = [];
+    }
+    if (document.querySelectorAll(".front.face.rodarpratras").length == qntCartas){
+        alert(`Parabéns, você acertou em ${cont} passos.`);
+    }
+ }
 
 // Esta função pode ficar separada do código acima, onde você preferir
 function comparador() { 
